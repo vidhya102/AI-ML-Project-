c58ab6af87e30c749fdb0166cfeb43a59e2f4ccd
vidhya102 <vidhyamhaske224@gmail.com>
2025-11-16 13:38:56 +0530

    Remove venv folder

--- venv/Lib/site-packages/gradio/_frontend_code/upload/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Upload } from "./Upload.svelte";
-export { default as ModifyUpload } from "./ModifyUpload.svelte";
-export { default as UploadProgress } from "./UploadProgress.svelte";
-export { create_drag } from "./utils";
--- venv/Lib/site-packages/gradio/templates/frontend/gradio.js
+++
@@ -1,1 +0,0 @@
-import("./assets/index-BvFm9VBJ.js");
